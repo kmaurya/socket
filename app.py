44e67b0c489e3d1398ed93f844a849da9840a8b9
Kavita Maurya <kavita.maurya@capgemini.com>
2021-04-03 22:53:00 +0530

    hh

--- app.py
+++ app.py
@@ -39,6 +39,6 @@ if __name__ == "__main__":
     host = '0.0.0.0'
     #port = 5000
 
-    httpd = simple_server.make_server(host, port, socket_io.run(app))
+    httpd = simple_server.make_server(host, port, app)
     #print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
